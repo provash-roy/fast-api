22741956341711e5fb245a8f0ff672c3df810d48
Provash-roy <provashray94@gmail.com>
2026-08-04 01:14:24 +0600

    Add database connection and session management

---
+++ src/todoapp/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from todoapp.core.config import settings
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "Production Todo API",
+    }
